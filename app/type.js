4b63fbcd4184e99340b54c43348daf183ff4b1e1
iigmir <roc120j@gmail.com>
2024-05-02 21:24:19 +0800

    Fix error

--- app/type.js
+++ app/type.js
@@ -2,11 +2,10 @@ import { get_news_by_type } from "../lib/fetch.js";
 import { get_news_links } from "../lib/parser.js";
 import { json_to_csv, write_file } from "../lib/fs.js";
 
-const write_file_action = (document) => {
-    const FILE = `./result/type-${Type}-at-page-${currentPage}.csv`;
-    write_file(FILE, json_to_csv(get_news_links(document)));
-};
-
 export const get_links_from_type = (Type = "latest", currentPage = "1") => {
+    const write_file_action = (document) => {
+        const FILE = `./result/type-${Type}-at-page-${currentPage}.csv`;
+        write_file(FILE, json_to_csv(get_news_links(document)));
+    };
     get_news_by_type( Type, currentPage ).then( write_file_action );
 };
